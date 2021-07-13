11db67e14e1b6ec052c994b8bf751df7295cc835
sharanya02 <sharanya02@rediffmail.com>
2021-07-13 18:51:29 +0530

    fix: changed db url

--- database/connection.js
+++ database/connection.js
@@ -6,12 +6,12 @@ dotEnv.config()
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  // }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 module.exports = sequelize
